053e693df99b530b303e79e2edb7b7a09bfbec59
IPv777 <19636346+IPv777@users.noreply.github.com>
2020-02-11 12:27:19 +0100

    ajout route iTopIdExists

--- html/api/index.php
+++ html/api/index.php
@@ -156,6 +156,7 @@ addRoute('post', '/listNagiosStates', 'listNagiosStates', 'readonly');
 addRoute('post', '/listNagiosObjects', 'listNagiosObjects', 'readonly');
 addRoute('post', '/listNagiosBackends', 'listNagiosBackends', 'readonly');
 addRoute('post', '/listHosts', 'listHosts', 'readonly');
+addRoute('post', '/iTopIdExists', 'iTopIdExists', 'readonly');
 
 //==============================================================================
 //eonweb
